6b299030f847bcfaa241ca396ffcfb1bd97cae7d
majadi <majadi@fsdhgroup.com>
2024-07-18 19:13:46 -0700

    started add task funct

--- src/pages/dashboard/dashboardPage.tsx
+++ src/pages/dashboard/dashboardPage.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
 const DashboardPage = () => {
+    const [addTask, setAddTask] = useState(false)
     const currentServicePopular = 0;
     const porpularServices = [
         {
@@ -24,35 +27,35 @@ const DashboardPage = () => {
                     style={{ minHeight: '20em', backgroundColor: '#134B70' }}>
                     <Card.Body className="d-flex text-light gap-3 flex-column align-items-center justify-content-center">
                         <div className="px-2 gap-1 d-flex flex-column m-0 w-100 align-items-center rounded py-2"
-                        style={{backgroundColor:'#508C9B'}}
+                            style={{ backgroundColor: '#508C9B' }}
                         >
                             <div className="d-flex gap-3 align-items-center justify-content-center">
-                            <i className="bi bi-people-fill" style={{ fontSize: '1.7em' }}></i>
+                                <i className="bi bi-people-fill" style={{ fontSize: '1.7em' }}></i>
                                 <h5 className="w-75">
-                                Connect with proffessionals
-                                arround your area.
+                                    Connect with proffessionals
+                                    arround your area.
                                 </h5>
                             </div>
 
                             <p className="px-3">
-                            Create a brief and get custom offers.
+                                Create a brief and get custom offers.
                             </p>
 
                         </div>
 
                         <div className="px-2 gap-1 d-flex flex-column m-0 w-100 align-items-center rounded py-2"
-                        style={{backgroundColor:'#17153B'}}
+                            style={{ backgroundColor: '#17153B' }}
                         >
                             <div className="d-flex gap-3 align-items-center justify-content-center">
-                            <i className="bi bi-card-checklist" style={{ fontSize: '1.7em' }}></i>
-                            
+                                <i className="bi bi-card-checklist" style={{ fontSize: '1.7em' }}></i>
+
                                 <h5 className="w-75">
-                                Tell us what you can do and how much you can do it for.
+                                    Tell us what you can do and how much you can do it for.
                                 </h5>
                             </div>
 
                             <p className="px-3">
-                            Create a list of services you can render.
+                                Create a list of services you can render.
                             </p>
 
                         </div>
@@ -62,7 +65,10 @@ const DashboardPage = () => {
             </div>
 
             <div className="w-100 text-center mt-3" >
-                <Button className="text-primary bg-light shadow-lg" style={{fontSize:'2em', height:'2em',width:'2em', borderRadius:'2em'}}>+</Button>
+                <Button
+                    onClick={() => setAddTask(true)}
+                    className="text-primary bg-light shadow-lg"
+                    style={{ fontSize: '2em', height: '2em', width: '2em', borderRadius: '2em' }}>+</Button>
                 <p className="mt-2 fw-bold">Add New</p>
             </div>
 
@@ -71,33 +77,35 @@ const DashboardPage = () => {
             </div> */}
             <div className="d-flex text-center p-3">
 
-<hr className="px-3 w-100" />
-</div>
+                <hr className="px-3 w-100" />
+            </div>
 
             <h3 className=" mt-2">See Services you may need.</h3>
             <div className="d-flex gap-3 w-100 mt-3  align-items-center"
-                        id="services"
-                        style={{overflowX: 'scroll', maxWidth: '100%' }}>
-                        {
-                            porpularServices[currentServicePopular].gigs.map((serv: any) => (
-                                <div className="d-flex flex-column align-items-center gap-2"
-                                    style={{ minWidth: '10em', maxHeight: '15em', minHeight: '15em' }}>
-                                    <Card className="shadow-sm border-0"
-                                        style={{ minWidth: '10em', minHeight: '14em', backgroundColor: serv.color }}>
-                                        <Card.Body className="d-flex text-light flex-column align-items-center justify-content-center">
-                                            <i className={` ${serv.icon}`} style={{ fontSize: '2em' }}></i>
+                id="services"
+                style={{ overflowX: 'scroll', maxWidth: '100%' }}>
+                {
+                    porpularServices[currentServicePopular].gigs.map((serv: any) => (
+                        <div className="d-flex flex-column align-items-center gap-2"
+                            style={{ minWidth: '10em', maxHeight: '15em', minHeight: '15em' }}>
+                            <Card className="shadow-sm border-0"
+                                style={{ minWidth: '10em', minHeight: '14em', backgroundColor: serv.color }}>
+                                <Card.Body className="d-flex text-light flex-column align-items-center justify-content-center">
+                                    <i className={` ${serv.icon}`} style={{ fontSize: '2em' }}></i>
 
-                                            <p className="niramit-semibold text-center"
-                                                style={{ textWrap: 'wrap', wordWrap: 'break-word' }}>{serv.title}</p>
-                                        </Card.Body>
-                                    </Card>
+                                    <p className="niramit-semibold text-center"
+                                        style={{ textWrap: 'wrap', wordWrap: 'break-word' }}>{serv.title}</p>
+                                </Card.Body>
+                            </Card>
 
-                                </div>
+                        </div>
 
-                            ))
-                        }
-                    </div>
+                    ))
+                }
+            </div>
+            <Modal show={addTask}>
 
+            </Modal>
         </div>
     )
 
