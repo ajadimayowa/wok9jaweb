ee8b86bde0c890d1f99bd2a039721d11ac2e40f4
majadi <majadi@fsdhgroup.com>
2024-08-23 08:23:29 -0700

    fixed envir for mail and funding

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,8 @@
 let dev = import.meta.env.DEV;
 let baseURL = import.meta.env.BASE_URL
 
-export const  fundingURL=import.meta.env.VITE_FUNDING_URL
-export const emailVerificationUrl = import.meta.env.VITE_APP_BASE_URL
+export let  fundingURL=import.meta.env.VITE_APP_FUNDING_URL
+export let emailVerificationUrl = import.meta.env.VITE_APP_BASE_URL
 
 const enviromentMode = ()=>{
     console.log(dev)
